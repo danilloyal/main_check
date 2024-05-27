3851a8527d3f62f9063b657154d31a015d73f3a5
Danillo Leal <tmdanillomartins@gmail.com>
2024-05-27 12:27:36 -0300

    estrutura inicial projeto Spring

---
+++ src/main/java/com/br/main_check/MainCheckApplication.java
@@ -0,0 +1,13 @@
+package com.br.main_check;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainCheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/main_check/MainCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.main_check;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
